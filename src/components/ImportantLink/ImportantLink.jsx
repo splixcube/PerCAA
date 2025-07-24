b1288cb5e0d2be24bc98b5577a667b91c4e93434
Nayan2812 <nayanmathur2812@gmail.com>
2025-07-24 10:00:08 +0530

    change links

--- src/components/ImportantLink/ImportantLink.jsx
+++ src/components/ImportantLink/ImportantLink.jsx
@@ -6,29 +6,29 @@ const ImportantBanner = () => {
     <div className="important-banner">
       <div className="important-label">IMPORTANT</div>
       <div className="marquee-container">
-        {/*<div className="marquee-content">
+        <div className="marquee-content">
           <a
-            href="https://cmt3.research.microsoft.com/PERCAA2024"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
             Paper Submission Link : Please Click Here To Submit Paper •
           </a>
           <a
-            href="https://cmt3.research.microsoft.com/PERCAA2024"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
             Paper Submission Link : Please Click Here To Submit Paper •
           </a>
           <a
-            href="https://cmt3.research.microsoft.com/PERCAA2024"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
             Paper Submission Link : Please Click Here To Submit Paper •
           </a>
-        </div>*/}
+        </div>
       </div>
     </div>
   );
