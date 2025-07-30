fee8836e3041b5e9fd39e1b01dee78a7b2596d75
Nayan2812 <nayanmathur2812@gmail.com>
2025-07-30 15:20:52 +0530

    CMT Message added

--- src/components/HomeContent/Home.jsx
+++ src/components/HomeContent/Home.jsx
@@ -88,6 +88,15 @@ function Home() {
               </p>
             </div>
           </div>
+          <div className="homeContent">
+            <br/>
+            <div className="paracontent">
+              <p>
+                <strong>*The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
+                </strong>
+              </p>
+            </div>
+          </div>
         </div>
         
         <div className="rightContainer">
