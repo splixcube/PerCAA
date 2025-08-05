70e6df8a52f915691d492d5ea838c70b7ac6607a
Nayan2812 <nayanmathur2812@gmail.com>
2025-08-05 10:05:44 +0530

    cmt content

--- src/components/HomeContent/Home.jsx
+++ src/components/HomeContent/Home.jsx
@@ -92,8 +92,7 @@ function Home() {
             <br/>
             <div className="paracontent">
               <p>
-                The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
-              
+                Acknowledgment: The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
               </p>
             </div>
           </div>
